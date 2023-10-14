53f47c86da3b9eccd170b63560d7f8c329749a98
tuanbui0509 <tuanbui0509@example.com>
2023-10-14 16:47:14 +0700

    add frontend ui

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/navbar/Navbar";
+import AddProduct from "./pages/add-product/AddProduct.page";
+import DeleteProduct from "./pages/delete-product/DeleteProduct.page";
+import Products from "./pages/products/Products.page";
+import EditProduct from "./pages/edit-product/EditProduct.page";
+import Home from "./pages/home/Home.page";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App: React.FC = () => {
+   return (
+      <div>
+         {/* Navbar */}
+         <Navbar />
+
+         {/* Wrapper */}
+         <div className="wrapper">
+            <Routes>
+               <Route path="/" element={<Home />} />
+               <Route path="/products">
+                  <Route index element={<Products />} />
+                  <Route path="add" element={<AddProduct />} />
+                  <Route path="edit/:id" element={<EditProduct />} />
+                  <Route path="delete/:id" element={<DeleteProduct />} />
+               </Route>
+            </Routes>
+         </div>
+      </div>
+   );
+};
 
 export default App;
